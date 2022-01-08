6cd4490cfa73cb5afeefc8ce15db6a23580a5aeb
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-01-08 01:22:30 -0600

    added current date js and taskAudit background test js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-var currentDate = moment().format("dddd, MMMM Do");
-console.log(currentDate);
+var setDate = function() {
+    var currentDate = moment().format("dddd, MMMM Do");
+
+    $("#currentDay").text(currentDate);
+}
+
+setDate();
+
+var taskAudit = function() {
+    var taskHour = $(".hour").text();
+
+    var hour = moment(taskHour, ["ha"]).format("HH");
+
+    if (hour > moment().hour()) {
+        $("textarea").addClass("bg-success")
+    };
+};
+
+taskAudit();
