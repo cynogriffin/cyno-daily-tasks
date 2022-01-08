b0960b2af9381fd35001a17a1b5a7b1a8cfbaa08
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-01-08 10:10:06 -0600

    added functionality to change bg based on each div's hour and the current time hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// set the current date at the top of the page
 var setDate = function() {
     var currentDate = moment().format("dddd, MMMM Do");
 
@@ -6,14 +7,22 @@ var setDate = function() {
 
 setDate();
 
+// audit the time blocks and change bg color according to current hour
 var taskAudit = function() {
-    var taskHour = $(".hour").text();
+    // loop through each .hour div
+    $(".hour").each(function() {
+        // convert the hour text to 24hr format
+        var taskHour = moment($(this).text(), ["ha"]).format("HH");
 
-    var hour = moment(taskHour, ["ha"]).format("HH");
-
-    if (hour > moment().hour()) {
-        $("textarea").addClass("bg-success")
-    };
+        // compare taskHour to current hour and change bg accordingly
+        if (taskHour > moment().format("HH")) {
+            $("#" + taskHour).addClass("future");
+        } else if (taskHour === moment().format("HH")) {
+            $("#" + taskHour).addClass("present");
+        } else {
+            $("#" + taskHour).addClass("past");
+        }
+    });
 };
 
 taskAudit();
